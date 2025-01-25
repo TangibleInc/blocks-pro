afeb9f4296ed58d30e18f81b44c1da66945f825b
eliot-akira <me@eliotakira.com>
2025-01-25 23:05:10 +0100

    Update local dev and test setup - Update dependencies - Test with PHP 7.4 and 8.4 - E2E tests with Playwright and WordPress Playground - GitHub Actions workflow

--- tangible-blocks-pro.php
+++ tangible-blocks-pro.php
@@ -2,18 +2,29 @@
 /**
  * Plugin Name: Tangible Blocks - Pro
  * Plugin URI: https://tangibleblocks.com/pro
- * Description: Extend Tangible Blocks with Pro features: third-party plugin integrations (Easy Digital Downloads, Events Calendar, Gravity Forms, LearnDash, LifterLMS, WooCommerce); Form module; Cloud access
+ * Description: Extend Tangible Blocks with Pro features: third-party plugin integrations - Easy Digital Downloads, Events Calendar, Gravity Forms, LearnDash, LifterLMS, WooCommerce
  * Version: 3.0.6
  * Author: Team Tangible
  * Author URI: https://teamtangible.com
  * License: GPLv2 or later
  */
+use tangible\framework;
+use tangible\updater;
 
 define( 'TANGIBLE_BLOCKS_PRO_VERSION', '3.0.6' );
 
-require_once __DIR__ . '/vendor/tangible/plugin-framework/index.php';
-require_once __DIR__ . '/vendor/tangible/plugin-updater/index.php';
-require_once __DIR__ . '/vendor/tangible/template-system-pro/index.php';
+$module_path = is_dir(
+  ($path = __DIR__ . '/../../tangible') // Module
+) ? $path : __DIR__ . '/vendor/tangible'; // Plugin
+
+require_once $module_path . '/framework/index.php';
+require_once $module_path . '/betterdash/index.php';
+require_once $module_path . '/betterlifter/index.php';
+require_once $module_path . '/template-system/index.php';
+require_once $module_path . '/template-system-pro/index.php';
+require_once $module_path . '/fields/index.php';
+require_once $module_path . '/fields-pro/index.php';
+require_once $module_path . '/updater/index.php';
 
 /**
  * Get plugin instance
@@ -25,8 +36,7 @@ function tangible_blocks_pro($instance = false) {
 
 add_action('plugins_loaded', function() {
 
-  $framework = tangible();
-  $plugin    = $framework->register_plugin([
+  $plugin    = framework\register_plugin([
     'name'           => 'tangible-blocks-pro',
     'title'          => 'Tangible Blocks Pro',
     'setting_prefix' => 'tangible_blocks_pro',
@@ -39,25 +49,21 @@ add_action('plugins_loaded', function() {
     'assets_url'     => plugins_url( '/assets', __FILE__ ),
   ]);
 
-  $plugin->register_dependencies([
-    'tangible-blocks/tangible-blocks.php' => [
-      'title' => 'Tangible Blocks',
-      'url' => 'https://tangibleblocks.com/',
-      'fallback_check' => function() {
-        return function_exists('tangible_blocks');
-      }
-    ]
+  framework\register_plugin_dependencies($plugin, [
+    'title' => 'Tangible Blocks',
+    'url' => 'https://tangibleblocks.com/',
+    'active' => function_exists('tangible_blocks'),
   ]);
 
   tangible_blocks_pro( $plugin );
 
-  tangible_plugin_updater()->register_plugin([
+  updater\register_plugin([
     'name' => $plugin->name,
     'file' => __FILE__,
     // 'license' => ''
   ]);
 
-  if (!$plugin->has_all_dependencies()) return;
+  if (!framework\has_all_plugin_dependencies($plugin)) return;
 
   // Features loaded will have in their local scope: $framework, $plugin, ...
 
